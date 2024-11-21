63ed4383be13803d6ba67afe58ee5022dece13e8
Hassan Rahman <hr-awesome@hotmail.com>
2024-11-20 19:00:02 -0500

    Add project files.

---
+++ OopExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ingresar Datos
+        }
+    }
+
+    class TarjetaCredito:Tarjeta
+    {
+
+    }
+
+    class TarjetaDebito:Tarjeta
+    {
+
+    }
+}
